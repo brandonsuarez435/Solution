247c8818173ba5f011aef3124e8368669b0b415d
Brandon Suárez <143466135+brandonsuarez435@users.noreply.github.com>
2025-02-12 08:16:16 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def josephus(ls, skip):
+    d = deque(ls)
+    survivor = None
+    
+    while d:
+        d.rotate(-skip)
+        survivor = d.popleft()  # Eliminamos el elemento seleccionado
+    
+    return survivor
+
+n = 7 
+k = 3
+people = list(range(1, n + 1))
+print("El sobreviviente es:", josephus(people, k))
